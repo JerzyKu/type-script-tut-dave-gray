b96dd093b265b46f02f68e5338e54bf25a12ed3d
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-01-13 21:19:24 +0100

    lesson 05

---
+++ LESSON05/build/js/copyright.js
@@ -0,0 +1,24 @@
+"use strict";
+// orgional js
+// const year = document.getElementById('year')
+// const thisYear = new Date().getFullYear()
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
+// my solution
+// const year = <HTMLElement>document.getElementById('year')
+// const thisYear = String(new Date().getFullYear())
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
+// 1st variation
+// let year: HTMLElement | null
+// year = document.getElementById('year')
+// let thisYear: string = String(new Date().getFullYear())
+// if (year){
+//     year.setAttribute("datetime", thisYear)
+//     year.textContent = thisYear
+// }
+// 2st variation
+const year = document.getElementById("year");
+const thisYear = String(new Date().getFullYear());
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
