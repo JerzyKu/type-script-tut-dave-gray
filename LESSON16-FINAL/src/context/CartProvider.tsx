7311c9528debe0ddbbd4f6126befcda1fe2b002d
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-02-15 21:17:18 +0100

    finish contex, end part one

--- LESSON16-FINAL/src/context/CartProvider.tsx
+++ LESSON16-FINAL/src/context/CartProvider.tsx
@@ -1,4 +1,9 @@
-import { useMemo, useReducer } from "react";
+import {
+  ReactElement,
+  createContext,
+  useMemo,
+  useReducer,
+} from "react";
 
 export type CartItemType = {
   sku: string;
@@ -105,16 +110,45 @@ const useCartContext = (initCartState: CartStateType) => {
     return previousValue + cartItem.qty;
   }, 0);
 
-  const totalPrice = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(
-    state.cart.reduce((previousValue , cartItem)  => {
-        return previousValue + (cartItem.qty * cartItem.price)
-    }, 0))
+  const totalPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(
+    state.cart.reduce((previousValue, cartItem) => {
+      return previousValue + cartItem.qty * cartItem.price;
+    }, 0)
+  );
 
-    const cart = state.cart.sort((a,b) => {
-        const itemA = Number(a.sku.slice(-4))
-        const itemb = Number(b.sku.slice(-4))
-        return itemA - itemb
-    })
+  const cart = state.cart.sort((a, b) => {
+    const itemA = Number(a.sku.slice(-4));
+    const itemb = Number(b.sku.slice(-4));
+    return itemA - itemb;
+  });
 
-    return {dispatch , REDUCER_ACTIONS, totalItems, totalPrice, cart}
+  return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart };
 };
+
+export type UseCartContextType = ReturnType<typeof useCartContext>;
+
+const initCartContextState: UseCartContextType = {
+  dispatch: () => {},
+  REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+  totalItems: 0,
+  totalPrice: "",
+  cart: [],
+};
+
+export const CartContext =
+  createContext<UseCartContextType>(initCartContextState);
+
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+export const CartProvider = ({ children }: ChildrenType): ReactElement => {
+  return (
+    <CartContext.Provider value={useCartContext(initCartState)}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContext
