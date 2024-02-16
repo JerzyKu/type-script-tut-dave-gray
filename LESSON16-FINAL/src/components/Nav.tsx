50738d92964befe4225f893dadcf332a035f947e
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-02-16 07:12:22 +0100

    in the middle

---
+++ LESSON16-FINAL/src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import { PropsType } from "./Header.tsx";
+
+const Nav = ({ viewCart, setViewCart }: PropsType) => {
+  const button = viewCart ? (
+    <button onClick={() => setViewCart(false)}>View Products</button>
+  ) : (
+    <button onClick={() => setViewCart(true)}>View Cart</button>
+  );
+
+  const content = <nav className="nav">{button}</nav>;
+
+  return  content;
+};
+
+export default Nav;
